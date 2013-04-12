6104dedb9eb09eea6122c07b50f36071d307c863
HentschelT <thomas.hentschel@novellus.com>
2013-04-12 01:19:03 -0700

    license cleanup

--- htmobile/src/main/java/net/hentschel/home/hottub/mobile/server/rest/Pump.java
+++ htmobile/src/main/java/net/hentschel/home/hottub/mobile/server/rest/Pump.java
@@ -2,14 +2,6 @@
  * Class: Pump
  * 
  * Created on Apr 9, 2013
- * 
- * (c) Copyright Novellus Systems, Inc., unpublished work, created 2003
- * All use, disclosure, and/or reproduction of this material is prohibited
- * unless authorized in writing.  All Rights Reserved.
- * Rights in this program belong to:
- * Novellus Systems, Inc.
- * 4000 N. First Street
- * San Jose, CA
  */
 package net.hentschel.home.hottub.mobile.server.rest;
 
